e57a4c1bdfa010bf1c8ed5062500d6146b2710d9
vladtf <vendeta0263@gmail.com>
2020-07-23 02:49:11 +0300

    Add news model

--- daspf_app/admin.py
+++ daspf_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from daspf_app.models import *
+
+admin.site.register(News)
--- daspf_app/models.py
+++ daspf_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    category = models.CharField(max_length=200, null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    visible = models.BooleanField(default=True)
+    views = models.IntegerField(default=0)
