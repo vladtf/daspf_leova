1dbd574081477104905b3f7dfd54e92b4828218c
vladtf <vendeta0263@gmail.com>
2020-07-23 02:32:51 +0300

    Add page layouts

--- daspf_app/views.py
+++ daspf_app/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     context = {}
-    return render(request, 'layouts/main.html', context=context)
+    return render(request, 'views/index.html', context=context)
+
+
+def contacts(request):
+    context = {}
+    return render(request, 'views/contacts.html', context=context)
