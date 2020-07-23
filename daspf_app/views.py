81692a9cc6fb9467f49e118e6beec77eccbb736a
vladtf <vendeta0263@gmail.com>
2020-07-23 07:44:19 +0300

    Add paginator

--- daspf_app/views.py
+++ daspf_app/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,8 +6,19 @@ from daspf_app.models import Post
 
 
 def index(request):
-    news = Post.objects.all().filter(visible=True).order_by('-created_at')
-    context = {'posts': news}
+    post_list = Post.objects.all().filter(visible=True).order_by('-created_at')
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(post_list, 5)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    context = {'posts': posts}
     return render(request, 'views/index.html', context=context)
 
 
