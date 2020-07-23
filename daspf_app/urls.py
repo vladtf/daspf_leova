ea6b7b0f5203b5ced22767e936d4b965c2708266
vladtf <vendeta0263@gmail.com>
2020-07-24 02:38:09 +0300

    Add events page

--- daspf_app/urls.py
+++ daspf_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('acasa/', views.home, name='home'),
     path('evenimente/', views.events, name='events'),
     path('contacte/', views.contacts, name='contacts'),
 ]
--- daspf_app/views.py
+++ daspf_app/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from daspf_app.models import Post
+from daspf_app.models import Post, Category
 
 
 def index(request):
@@ -22,11 +22,29 @@ def index(request):
     return render(request, 'views/index.html', context=context)
 
 
+def events(request):
+    post_list = Post.objects.all().filter(category=Category.objects.get(name='Evenimente'), visible=True).order_by(
+        '-created_at')
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(post_list, 3)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    context = {'posts': posts}
+    return render(request, 'views/events.html', context=context)
+
+
+def home(request):
+    context = {}
+    return render(request, 'views/home.html', context=context)
+
+
 def contacts(request):
     context = {}
     return render(request, 'views/contacts.html', context=context)
-
-
-def events(request):
-    context = {}
-    return render(request, 'views/events.html', context=context)
