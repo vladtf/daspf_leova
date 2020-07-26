a405bebffe7d844472ce1903fb44f3cc88073b56
vladtf <vendeta0263@gmail.com>
2020-07-26 20:15:52 +0300

    Refactored sidebar

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+// tinymce.init({selector: 'textarea'});
+
+if (document.getElementById('id_body') !== null) {
+    CKEDITOR.replace('id_body', {
+        extraPlugins: ['autogrow', 'image2', 'indentlist']
+    });
+}
+
+$('.carousel').on('slide.bs.carousel', function (event) {
+    var height = $(event.relatedTarget).height();
+    var $innerCarousel = $(event.target).find('.carousel-inner');
+
+    $innerCarousel.animate({
+        height: height
+    });
+});
