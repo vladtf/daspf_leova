5b714ed1fda6f1e5f6d5348391b06d332ec796aa
vladtf <vendeta0263@gmail.com>
2020-09-04 01:05:15 +0300

    Reformat main.js

--- static/js/main.js
+++ static/js/main.js
@@ -8,78 +8,87 @@ import 'popper.js';
 import './fit-text'
 
 
-if ($("#id_body").length) {
-    CKEDITOR.replace('id_body', {
-        extraPlugins: ['autogrow', 'indentlist', 'image2'],
+function setupCKEditor() {
+    if ($("#id_body").length) {
+        CKEDITOR.replace('id_body', {
+            extraPlugins: ['autogrow', 'indentlist', 'image2'],
+        });
+    }
+}
+
+function updateImagesSliderHeight() {
+    $('.carousel').on('slide.bs.carousel', function (event) {
+        var height = $(event.relatedTarget).height();
+        var $innerCarousel = $(event.target).find('.carousel-inner');
+
+        $innerCarousel.animate({
+            height: height
+        });
     });
 }
 
-$('.carousel').on('slide.bs.carousel', function (event) {
-    var height = $(event.relatedTarget).height();
-    var $innerCarousel = $(event.target).find('.carousel-inner');
+function addExtraImagesField() {
+    let form_count = Number($("[name=form-TOTAL_FORMS]").val());
 
-    $innerCarousel.animate({
-        height: height
+    $("#add_extra").click(function () {
+
+        let image_field = $(
+            '<div class="col-md-12 py-2 form-group image-field">' +
+            '<label for="id_form-' + form_count + '-image">Image: </label>' +
+            '<input type="file" name="form-' + form_count + '-image" accept="image/*" id="id_form-' + form_count + '-image">' +
+            '<input type="hidden" name="form-' + form_count + '-id" id="id_form-' + form_count + '-id">' +
+            '</div>'
+        );
+
+        $('.image-field').last().after(image_field);
+
+        $("[name=form-TOTAL_FORMS]").val(++form_count);
     });
-});
+}
 
-$('.sidebar-card-text').fitText();
+function updateTextAreaHeight() {
+    $("textarea").each(function (textarea) {
+        $(this).height($(this)[0].scrollHeight);
 
-let form_count = Number($("[name=form-TOTAL_FORMS]").val());
-
-$("#add_extra").click(function () {
-
-    let image_field = $(
-        '<div class="col-md-12 py-2 form-group image-field">' +
-        '<label for="id_form-' + form_count + '-image">Image: </label>' +
-        '<input type="file" name="form-' + form_count + '-image" accept="image/*" id="id_form-' + form_count + '-image">' +
-        '<input type="hidden" name="form-' + form_count + '-id" id="id_form-' + form_count + '-id">' +
-        '</div>'
-    );
-
-    $('.image-field').last().after(image_field);
-
-    $("[name=form-TOTAL_FORMS]").val(++form_count);
-});
+        $(this).on("keydown keypress change", function () {
+            $(this).height(0);
+            $(this).height(this.scrollHeight + 30);
+        })
+    });
+};
 
 
-$("textarea").each(function (textarea) {
-    $(this).height($(this)[0].scrollHeight);
+function configCaptcha() {
+    $('.captcha').click(function () {
+        update_captcha()
+    });
 
-    $(this).on("keydown keypress change", function () {
-        $(this).height(0);
-        $(this).height(this.scrollHeight + 30);
+    $('#id_captcha_1').after('<button class=\'js-captcha-refresh btn btn-outline-danger ml-2\' type="button" id="re_captcha"><i class="fas fa-undo"></i></button>')
+    $('#re_captcha').click(function () {
+        update_captcha()
     })
-});
+}
 
-$('.captcha').click(function () {
-    update_captcha()
-});
-
-$('#id_captcha_1').after('<button class=\'js-captcha-refresh btn btn-outline-danger ml-2\' type="button" id="re_captcha"><i class="fas fa-undo"></i></button>')
-$('#re_captcha').click(function () {
-    update_captcha()
-})
-
-var update_captcha = function () {
+function update_captcha() {
     $.getJSON("/captcha/refresh/", function (result) {
         $('.captcha').attr('src', result['image_url']);
         $('#id_captcha_0').val(result['key'])
     });
 }
 
-if ($('#mapid').length) {
-    var mymap = L.map('mapid').setView([46.47715, 28.249547], 16);
+function setMapWidget() {
+    if ($('#mapid').length) {
+        var mymap = L.map('mapid').setView([46.47715, 28.249547], 16);
 
-    mymap.addControl(new L.Control.Fullscreen());
+        mymap.addControl(new L.Control.Fullscreen());
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(mymap);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(mymap);
 
-    var marker = L.marker([46.47715, 28.249547]).addTo(mymap);
-    marker.bindPopup("Directia Asistenta Sociala Leova");
-
+        var marker = L.marker([46.47715, 28.249547]).addTo(mymap);
+        marker.bindPopup("Directia Asistenta Sociala Leova");
+    }
 }
 
 $('#hide-editor-check').click(function () {
@@ -116,5 +125,13 @@ function showScrollButton() {
 }
 
 $(function () {
+    $('.sidebar-card-text').fitText();
+
     showScrollButton();
+    updateTextAreaHeight();
+    setupCKEditor();
+    updateImagesSliderHeight();
+    addExtraImagesField();
+    configCaptcha();
+    setMapWidget();
 });
