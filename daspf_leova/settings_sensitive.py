dc096914726b13d52539e53518882dc0f45dc5a3
vladtf <vendeta0263@gmail.com>
2020-09-19 19:45:45 +0300

    Email smtp settings

--- daspf_leova/settings_sensitive.py
+++ daspf_leova/settings_sensitive.py
@@ -1,3 +1,8 @@
 # IMGUR_CLIENT_ID = '2982eb388d4311c'
 # IMGUR_CLIENT_SECRET = '9d2c55c7d6fa8ac0a60ddcb951d17bc62b75f662'
 #
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'postmaster@mg.daspf.me'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_USE_TLS = True
